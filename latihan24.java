182d553a144aef53ba5a5986a5e6de0f58cabde9
Bara Abiyyi <122656775+BaraAbiyyi@users.noreply.github.com>
2024-11-04 10:50:02 +0700

    Add files via upload

---
+++ latihan24.java
@@ -0,0 +1,33 @@
+package pertemuan5;
+import java.util.Scanner;
+
+public class latihan24 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Masukkan nilai pertama: ");
+            int nilaiPertama = scanner.nextInt();
+            System.out.print("Masukkan nilai kedua: ");
+            int nilaiKedua = scanner.nextInt();
+
+            if (nilaiPertama > nilaiKedua) {
+                System.out.println("Hasil: " + nilaiPertama + " Lebih besar dari " + nilaiKedua);
+            } else if (nilaiPertama < nilaiKedua) {
+                System.out.println("Hasil: " + nilaiPertama + " Lebih kecil dari " + nilaiKedua);
+            } else {
+                System.out.println("Hasil: " + nilaiPertama + " Sama dengan " + nilaiKedua);
+            }
+
+            System.out.print("Ulangi Aktifitas? (Ya/Tidak): ");
+            String pilihan = scanner.next();
+            if (!pilihan.equalsIgnoreCase("Ya")) {
+                break;
+            }
+        }
+        System.out.println("BUILD SUCCESSFUL");
+        scanner.close();
+    }
+} 
+
